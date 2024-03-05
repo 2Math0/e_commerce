aaf7f049f5942689fdfabd4182901b0cec6c9d84
2Math0 <toto.mm50.tm@gmail.com>
2024-03-05 17:13:24 +0200

    re-design product

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:e_commerce/domain/product.dart';
 import 'package:e_commerce/presentation/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -12,87 +15,110 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 0.0, // Remove default elevation
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
-      margin: const EdgeInsets.all(8.0),
-      elevation: 5.0,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ProductDetailScreen(product: product),
-            ),
-          );
-        },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Hero(
-              tag: 'productImage_${product.id}',
-              child: ClipRRect(
-                borderRadius:
-                    const BorderRadius.vertical(top: Radius.circular(16.0)),
-                child: CachedNetworkImage(
-                  imageUrl: product.image,
-                  height: 90,
-                  width: 90,
-                  fit: BoxFit.contain,
-                ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.green.withOpacity(0.3),
+                // Set color and transparency
+                borderRadius: BorderRadius.circular(16.0),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    product.title,
-                    overflow: TextOverflow.clip,
-                  ),
-                  const SizedBox(height: 8.0),
-                  Text(
-                    '\$${product.price.toString()}',
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.green,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProductDetailScreen(product: product),
                     ),
-                  ),
-                  const SizedBox(height: 8.0),
-
-                ],
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Hero(
+                      tag: 'productImage_${product.id}',
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(16.0)),
+                        child: CircleAvatar(
+                          minRadius: 65,
+                          backgroundColor: Colors.white,
+                          child: CachedNetworkImage(
+                            imageUrl: product.image,
+                            height: 90,
+                            width: 90,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text(
+                              product.title,
+                              softWrap: true,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              '\$${product.price.toString()}',
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.green,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // Favorite button
+                        IconButton(
+                          icon: const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {
+                            // Handle favorite button tap
+                          },
+                        ),
+                        // Cart button
+                        IconButton(
+                          icon: const Icon(Icons.shopping_cart),
+                          onPressed: () {
+                            _addToCart(product);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('${product.title} added to cart'),
+                                duration: const Duration(seconds: 2),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-            const Spacer(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Favorite button
-                IconButton(
-                  icon: const Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {
-                    // Handle favorite button tap
-                  },
-                ),
-                // Cart button
-                IconButton(
-                  icon: const Icon(Icons.shopping_cart),
-                  onPressed: () {
-                    _addToCart(product);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('${product.title} added to cart'),
-                        duration: const Duration(seconds: 2),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
